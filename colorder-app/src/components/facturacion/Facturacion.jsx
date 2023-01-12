47947b8a6bfcd55a534f5ebd38b402cf7f930914
Nahuel Caceres <nahue_cge@hotmail.com>
2023-01-12 15:21:17 -0300

    asd

--- colorder-app/src/components/facturacion/Facturacion.jsx
+++ colorder-app/src/components/facturacion/Facturacion.jsx
@@ -5,24 +5,40 @@ import TableShow from '../tables/TableShow'
 const Facturacion = () => {
   const [data,setData] = useState([])
   const [pendientes, setPendientes] = useState(0)
-  const [checked,setChecked] = useState(false)
+  const [checked,setChecked] = useState(false) //
+  // const [searchParams, setSearchParams] = useSearchParams();
 
   useEffect(()=>{
-      
+      const searchParams = new URLSearchParams({
+        factura:checked?1:0
+        
+      })
+  
+
     const fetchData = async () => {
-      axios.get(`/api/pedidos?completado=0&factura=${checked?1:0}`)
+      axios.get(`/api/pedidos?completado=0&${searchParams}`)
       .then(res=>{
           setData(res.data)
           const arr_pendientes = data.filter(i => i.completado == false)
           setPendientes(arr_pendientes.length)
       })            
     };
+
+
+    axios.get(`/api/pedidos?completado=0&${searchParams}`)
+    .then(
+      response => {
+        console.log(response)
+        setData(response.data)
+      }
+      )
+    .catch(error => console.log(error));
     const interval = setInterval(fetchData, 5000);
     return () => clearInterval(interval);
-  },[data])
-      
+  },[checked])
+  
   return (
-    <TableShow  setChecked={setChecked} datos={data} titulo="Pedidos" subtitulo="Pr" > </TableShow>
+    <TableShow  setChecked={setChecked} datos={data} titulo="Pedidos" subtitulo="" > </TableShow>
   )
 }
 
