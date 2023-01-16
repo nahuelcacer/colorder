4ee078eed3b8c9067b2d3e5bd11d0dc16b0e6cc6
nahuelcace <nahue_cge@hotmail.com>
2023-01-16 00:35:31 -0300

    pedido detalle

--- colorder-app/src/components/facturacion/MostrarPedidoFacturacion.jsx
+++ colorder-app/src/components/facturacion/MostrarPedidoFacturacion.jsx
@@ -1,8 +1,9 @@
-import { Modal, Typography } from "@mui/material"
-import { Box } from "@mui/system";
+import { Button, Modal, Paper, Table, TableBody, TableCell, TableHead, TableRow, Typography } from "@mui/material"
 import axios from "axios";
 import { useState } from "react";
-import StyleModal from "../../tools/styleModals";
+import getTotalCost from "../../tools/getTotalCost";
+import StyleModal from '../../tools/styleModals';
+
 
 const MostrarPedidoFacturacion = ({preparar, setPreparar}) => {
     const [pedido,setPedido] = useState({})
@@ -30,14 +31,73 @@ const MostrarPedidoFacturacion = ({preparar, setPreparar}) => {
             aria-labelledby="modal-modal-title"
             aria-describedby="modal-modal-description"
         >
-            <Box sx={StyleModal}>
-                {
-                preparar.pedido != null ?
-                <Box>
-                    {preparar.pedido.cliente.nombre}
-                </Box>:<></>
-                }
-            </Box>
+            <Paper sx={StyleModal}>
+                <Typography sx={{mb:2}} id="modal-modal-title" variant="h6" component="h2">
+                    CLIENTE
+                </Typography>
+                <Typography   >
+                    {preparar.on ? preparar.pedido.cliente.nombre : ''} 
+                </Typography>
+                <Typography   >
+                    {preparar.on ? preparar.pedido.cliente.dni : ''} 
+                </Typography>
+                <Typography sx={{mb:2}} id="modal-modal-title" variant="h6" component="h2">
+                    PEDIDO
+                </Typography>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell >Producto</TableCell>
+                            <TableCell >Precio</TableCell>
+                            <TableCell >Cantidad</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {
+                            preparar.on
+                            ?
+                            preparar.pedido.orderproduct.map(item=>{
+                                return(
+                                    <TableRow>
+                                        <TableCell align='center' sx={{borderBottom:'none'}}>{item.producto.nombre}</TableCell>
+                                        <TableCell align='center' sx={{borderBottom:'none'}}>{item.producto.precio}</TableCell>
+                                        <TableCell align='center' sx={{borderBottom:'none'}}>{item.cantidad}</TableCell>
+                                        
+
+                                    </TableRow>
+
+                                )
+                            })
+                            :
+                            ''
+                        }
+                        <TableRow  >
+                            <TableCell colspan={2} sx={{borderBottom:'none'}}>
+                                Total
+                            </TableCell>
+                            <TableCell align='center' sx={{borderBottom:'none'}}>
+                                {
+                                    preparar.on 
+                                    ?
+                                    getTotalCost(preparar.pedido.orderproduct)
+                                    :
+                                    ''
+                                }
+                            </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <Button color="primary">
+                                Facturado
+                            </Button>
+                        </TableRow>
+
+                    </TableBody>
+                    
+
+                </Table>
+                
+                
+            </Paper>
         </Modal>
     )
 }
